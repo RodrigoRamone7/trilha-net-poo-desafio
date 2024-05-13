2b2f6eea6db1a7b33f4909d8a6b716e9e8a7279b
RodrigoRamone7 <rodrigo_wow7@hotmail.com>
2024-05-13 15:56:07 -0300

    Desafio completado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphone = new Iphone("(61) 98764-1232", "15 Pro Max", "356303486840547", 128);
+Nokia nokia = new Nokia("(61) 98544-2562", "Sansung Galaxy S24", "356303486840547", 512);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+nokia.Ligar();
+nokia.ReceberLigacao();
+
+iphone.InstalarAplicativo("Calculadora");
+nokia.InstalarAplicativo("Câmera");
